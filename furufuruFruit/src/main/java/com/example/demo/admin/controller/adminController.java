7d346e28c4ee201a7a8508f47cfb1905e1bd587a
Ndonghyeon <nam990907@naver.com>
2024-09-03 15:37:32 +0900

    adminController make 2024-09-03

--- furufuruFruit/src/main/java/com/example/demo/admin/controller/adminController.java
+++ furufuruFruit/src/main/java/com/example/demo/admin/controller/adminController.java
@@ -1,5 +1,26 @@
 package com.example.demo.admin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
 public class adminController {
+
+
+@GetMapping("/admin_main")
+public String admin() {
+
+	return "admin/admin_sub_center";
+
+}
+
+@GetMapping("/admin_sub_center_detail")
+public String subCenterdetail() {
+
+	return "admin/admin_sub_center_detail";
+}
+
+
 
 }
