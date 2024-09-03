7415fb186e034bae3940ba05b252138be5cbf827
Soon-L <lesoon33@gmail.com>
2024-09-03 15:11:51 +0900

    기능구현 연습중~.~

--- furufuruFruit/src/main/java/com/example/demo/admin/controller/adminController.java
+++ furufuruFruit/src/main/java/com/example/demo/admin/controller/adminController.java
@@ -1,5 +1,21 @@
 package com.example.demo.admin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class adminController {
+	
+	@GetMapping("/master")
+	public String admin() {
+		
+		return "admin/admin_main";
+	}
+	
+	@GetMapping("/admin_sub_user")
+	public String admin_sub_center() {
+		
+		return "admin/admin_sub_user";
+	}
 
 }
