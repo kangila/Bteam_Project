362b4c61583a923b8c9e3ed8ac26aec18242dff9
HMG0807 <besil1317@naver.com>
2024-09-06 12:14:03 +0900

    2024-09-06 한민기 4차병합 삭제 엔티티 	recommend 	coupon 	wishlist 그외 엔티티 수정

--- furufuruFruit/src/main/java/com/example/demo/Entity/buyuser.java
+++ furufuruFruit/src/main/java/com/example/demo/Entity/buyuser.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,17 +22,56 @@ public class buyuser {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer buyuserId;	
+	
+	@Column(nullable = false)
 	private String id;
+	
+	@Column(nullable = false)
 	private String pw;
+	
+	@Column(nullable = false)
 	private String email;
+	
+	@Column(nullable = false)
 	private String name;
+	
+	@Column(nullable = false)
 	private String tell;
+	
+	@Column(nullable = false)
 	private String address;
+	
+	@Column(nullable = false)
 	private String gender;
 
+	@Column(nullable = false)
 	private LocalDateTime registerDate;
 	
 	@OneToMany(mappedBy = "buyuser", cascade = CascadeType.REMOVE)
-	private List<servicecenterquestion> questionList;
+	private List<cart> cartList;
+	
+	@OneToMany(mappedBy = "buyuser", cascade = CascadeType.REMOVE)
+	private List<servicecenterquestion> servicecenterquestionList;
+	
+	@OneToMany(mappedBy = "buyuser", cascade = CascadeType.REMOVE)
+	private List<productquestion> productquestionList;
+	
+	@OneToMany(mappedBy = "buyuser", cascade = CascadeType.REMOVE)
+	private List<prductorder> prductorderList;
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- furufuruFruit/src/main/java/com/example/demo/Entity/productquestion.java
+++ furufuruFruit/src/main/java/com/example/demo/Entity/productquestion.java
@@ -2,10 +2,15 @@ package com.example.demo.Entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,11 +23,29 @@ public class productquestion {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer pquestionId;
 	
-	private Integer buyuserId;
+	@Column(nullable = false)
 	private String title;
+	
+	@Column(nullable = false)
 	private String contents;
+	
+	@Column(nullable = false)
 	private LocalDateTime questionDate;
 	
+	@ManyToOne
+	@JoinColumn(name="buyuserId")
+	private buyuser buyuser;
 	
+	@OneToOne(mappedBy = "productquestion", cascade = CascadeType.REMOVE)
+	private productanswer productanswer;
 	
 }
+
+
+
+
+
+
+
+
+
