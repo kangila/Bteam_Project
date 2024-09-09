e4ac252ada8dc19948ea57a91cbd7c8b94e405a6
HMG0807 <besil1317@naver.com>
2024-09-09 10:20:14 +0900

    2024-09-09 한민기 5차병합 고객센터 수정, 삭제 구현

--- furufuruFruit/src/main/java/com/example/demo/ServiceCenter/controller/ServiceCenterController.java
+++ furufuruFruit/src/main/java/com/example/demo/ServiceCenter/controller/ServiceCenterController.java
@@ -1,8 +1,8 @@
 package com.example.demo.ServiceCenter.controller;
 
-import java.security.Principal;
 
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,11 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.Entity.servicecenterquestion;
-import com.example.demo.ServiceCenter.AnswerForm;
 import com.example.demo.ServiceCenter.QuestionForm;
 import com.example.demo.ServiceCenter.Exception.UserException;
 import com.example.demo.ServiceCenter.Service.QuestionService;
-
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -32,37 +30,71 @@ public class ServiceCenterController {
 
 		model.addAttribute("paging", paging);
 		
-		return "csc/CSC_form";
+		return "csc/CSC_List";
 	}
 	
 	
-	
-	@GetMapping(value = "/detail/{id}")
-	public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) 
-			throws UserException {
+	//한민기 작업
+	@GetMapping(value = "/csc/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id, QuestionForm questionForm) throws UserException {
 		servicecenterquestion q = this.qr.getQuestion(id);
 		model.addAttribute("question",  q);
-		return "inquiry";
+		return "csc/CSC_Detail";
 		
 	}
 	
 	
-	@GetMapping("/csc/inquiry")
-	public String questionCreate(QuestionForm questionForm) {
+	@GetMapping("/csc/form")
+	public String questionForm(QuestionForm questionForm) {
 
-		return "csc/CSC_form2";
+		return "csc/CSC_Form";
 	}
-
 	
-	@PostMapping("/csc/inquiry")
+	@PostMapping("/csc/form")
 	public String questionCreate(@Valid QuestionForm questionForm, 
 				BindingResult bindingResult) throws UserException {
 		
 		if(bindingResult.hasErrors()) {
-			return "csc/CSC_form2";
+			return "csc/CSC_Form";
 		}
 		
 		this.qr.create(questionForm.getTitle(), questionForm.getContents());
+		
+		return "redirect:/csc";
+	}
+	
+	//한민기 작업
+	@GetMapping("/csc/modify/{id}")
+	public String questionModify(QuestionForm questionForm, @PathVariable("id") Integer id) throws UserException {
+		
+		servicecenterquestion q = this.qr.getQuestion(id);
+		questionForm.setTitle(q.getTitle());
+		questionForm.setContents(q.getContents());
+		
+		return "csc/CSC_Form";
+		
+	}
+	
+	@PostMapping("/csc/modify/{id}")
+	public String questionModify(@Valid QuestionForm questionForm, 
+			@PathVariable("id") Integer id, BindingResult bindingResult) throws UserException {
+		
+		if(bindingResult.hasErrors()) {
+			return "csc/CSC_Form";
+		}
+		
+		servicecenterquestion q = this.qr.getQuestion(id);
+		this.qr.modify(q, questionForm.getTitle(), questionForm.getContents());
+		
+		return "redirect:/csc/detail/{id}";
+	}
+	
+	@GetMapping("/csc/delete/{id}")
+	public String questionDelete(QuestionForm questionForm, @PathVariable("id") Integer id) throws UserException {
+		
+		servicecenterquestion q = this.qr.getQuestion(id);
+		this.qr.delete(q);
+		
 		return "redirect:/csc";
 	}
 	
