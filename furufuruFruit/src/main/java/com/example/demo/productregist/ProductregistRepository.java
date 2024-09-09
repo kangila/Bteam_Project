2588073628760ebbc97217efae947e70bb045336
kangila <ia326466@gmail.com>
2024-09-09 14:23:20 +0900

    pruduct name change

--- furufuruFruit/src/main/java/com/example/demo/productregist/ProductregistRepository.java
+++ furufuruFruit/src/main/java/com/example/demo/productregist/ProductregistRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.Entity.product;
 
 // product 엔티티에 대한 CRUD(생성, 읽기, 업데이트, 삭제) 작업을 수행함. 
-public interface ProductRepository extends JpaRepository<product, Integer> {
+public interface ProductregistRepository extends JpaRepository<product, Integer> {
 
 	
 	
