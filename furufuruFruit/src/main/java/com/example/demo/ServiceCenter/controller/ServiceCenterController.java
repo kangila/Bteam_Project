faa709768080691a5fee4cdbb14f246f9832c542
kangila <ia326466@gmail.com>
2024-09-10 13:55:50 +0900

    2024-09-10 깃허브 수리 중

--- furufuruFruit/src/main/java/com/example/demo/ServiceCenter/controller/ServiceCenterController.java
+++ furufuruFruit/src/main/java/com/example/demo/ServiceCenter/controller/ServiceCenterController.java
@@ -20,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
-public class ServiceCenterController {
+public class ServiceCenterController {  
 	
 	private final QuestionService qr;
 	
